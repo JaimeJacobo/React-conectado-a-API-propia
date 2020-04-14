fa09a1eb7e6b8afd935fac24acbc0a3dda2fb6aa
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-04-14 15:32:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [myState, updateMyState] = useState({
+    title: '',
+    poster: '',
+    comment: ''
+  })
+
+  const submitInfo = async (event)=>{
+    event.preventDefault();
+    await fetch('https://prueba-deployment.herokuapp.com/crear-jugadora', {
+      method: 'POST',
+      // mode: 'no-cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        nombre: '123',
+        apellido: '123',
+        nacionalidad: '123',
+        ranking: '123'
+      })
+    })
+
+    console.log('Fetch realizado con exito')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hola soy el componente App</h1>
+
+      <form onSubmit={(event)=>submitInfo(event)}>
+
+        <label htmlFor="title">title:</label>
+        <input type="text" id="title" name="title" value={myState.title} onChange={(event)=>updateMyState({...myState, title: event.target.value})}/>
+        <br />
+
+        <label htmlFor="poster">poster:</label>
+        <input type="text" id="poster" name="poster" value={myState.poster} onChange={(event)=>updateMyState({...myState, poster: event.target.value})}/>
+        <br />
+
+        <label htmlFor="comment">comment:</label>
+        <input type="text" id="comment" name="comment" value={myState.comment} onChange={(event)=>updateMyState({...myState, comment: event.target.value})}/>
+        <br />
+
+
+        <button type="submit">Submit</button>
+
+      </form>
+
     </div>
   );
 }
